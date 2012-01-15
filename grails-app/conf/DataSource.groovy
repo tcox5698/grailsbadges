aefc69764efcb835895778b41e64ba188a8bf80b
Tom Cox <tcox56_98@yahoo.com>
2012-01-15 00:04:36 -0600

    ys

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -37,7 +37,7 @@ environments {
             driverClassName = "org.postgresql.Driver"
             dialect = "org.hibernate.dialect.PostgreSQLDialect"  
             
-            System.out.println("YUNGO URL: " + System.getenv())
+            System.out.println("YUNGO URL: " + ${DATABASE_URL})
             
             System.out.println("BONGO URL: " + System.env.DATABASE_URL)
             
