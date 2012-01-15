79e80fcbf7ab85efd0d4a41e7973b5605f16ba09
Tom Cox <tcox56_98@yahoo.com>
2012-01-15 17:14:18 -0600

    db script cleanup

---
+++ dbscripts/db_backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export PGPASSWORD="password"
+export DBWORK_DIR="./dbwork"
+export CUR_DATE=`date "+%Y%m%d-%H:%M:%S"`
+export TARGET_DB="$1"
+
+if [ -f $DBWORK_DIR/$TARGET_DB.bak ] ; then
+  mv $DBWORK_DIR/$TARGET_DB.bak $DBWORK_DIR/x$TARGET_DB.bak.$CUR_DATE
+fi
+
+pg_dump -U postgres -f $DBWORK_DIR/$TARGET_DB.bak $TARGET_DB
+
+echo "$TARGET_DB has been backed up"
