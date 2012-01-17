1e0e5f7028e9f963f2b08dc1a3eb27e16218204f
Tom Cox <tcox56_98@yahoo.com>
2012-01-16 23:12:44 -0600

    heroku_backup script

--- dbscripts/heroku_backup.sh
+++ dbscripts/heroku_backup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+export DBWORK_DIR="./dbwork"
+export CUR_DATE=`date "+%Y%m%d-%H:%M:%S"`
+export TARGET_DB="$1"
 
-heroku pgbackups:capture --app $1
+if [ -f $DBWORK_DIR/$TARGET_DB.dump ] ; then
+  mv $DBWORK_DIR/$TARGET_DB.dump $DBWORK_DIR/x$TARGET_DB.dump.$CUR_DATE
+fi
+
+heroku pgbackups:capture --app $TARGET_DB
+curl -o $DBWORK_DIR/$TARGET_DB.dump `heroku pgbackups:url --app $TARGET_DB`
+
+echo "$TARGET_DB heroku db dumped to $DBWORK_DIR/$TARGET_DB.dump"
