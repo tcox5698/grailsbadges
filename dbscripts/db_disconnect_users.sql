da8c62143b7aa47d1f3bbe0fb0cccf4b5d4f5c74
Tom Cox <tcox56_98@yahoo.com>
2012-01-17 22:13:48 -0600

    moved out db scripts

--- dbscripts/db_disconnect_users.sql
+++
@@ -1,2 +0,0 @@
-SELECT procpid, (SELECT pg_terminate_backend(procpid)) as killed from pg_stat_activity
-   WHERE current_query LIKE '<IDLE>';
--- dbscripts/heroku_txf_prod_to_stage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-heroku pgbackups:capture --app meritprod
-heroku pgbackups:restore DATABASE `heroku pgbackups:url --app meritprod` --app meritstage
